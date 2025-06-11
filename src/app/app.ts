1e6247dd991911725d6db1d882b3fafad1edbbde
Dolecki Patryk <pdolecki.it@gmail.com>
2025-06-11 21:04:22 +0200

    1: Added Angular Material, with custom theme and initial dark and light modes

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, effect, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  template: ` <router-outlet /> `,
+  imports: [RouterOutlet, MatSlideToggleModule],
+  template: `
+    <mat-slide-toggle
+      class="example-margin"
+      [checked]="darkMode()"
+      (change)="darkMode.set($event.checked)"
+    >
+      Dark Mode
+    </mat-slide-toggle>
+    <router-outlet />
+  `,
   styles: [],
 })
 export class App {
+  protected readonly darkMode = signal<boolean>(true);
+
+  protected switchTheme = effect(() => {
+    const darkMode = this.darkMode();
+    document.body.classList.toggle('darkMode', darkMode);
+  });
 }
