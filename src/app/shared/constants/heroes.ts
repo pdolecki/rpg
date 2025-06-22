2fd0240ecfe01e247fb18c52c823653b3682d77a
Dolecki Patryk <pdolecki.it@gmail.com>
2025-06-22 09:25:59 +0200

    15: Added some heroes descriptions

--- src/app/shared/constants/heroes.ts
+++ src/app/shared/constants/heroes.ts
@@ -3,20 +3,28 @@ import { Hero } from '../interfaces/features';
 export const HEROES: Hero[] = [
   {
     name: 'Victor',
-    description: 'Tutaj będzie krótka historia postaci... kiedyś',
+    description:
+      'Czarodziej metalu, niepokorny geniusz i uosobienie chaosu. Urodzony w Triumwiracie, od zawsze igrał z granicami magii i rozsądku. Charyzmatyczny, błyskotliwy i kompletnie nieprzewidywalny, stał się cieniem u boku księżnej Muriel, wspierając ją w imię szaleńczych ideałów. Zauroczony wizją nowego ładu, nie zauważył, że sam stał się narzędziem w rękach złych mocy. W przypływie obłędu próbował zabić wszystkich — łącznie z księżną — i zginął z rąk jej straży, kończąc historię jako ostrze chaosu, które samo siebie przecięło, bądź też jak GM zauważył słusznie - dildo w dupie jakiegoś demona.',
     url: '/heroes/victor.png',
   },
   {
     name: 'Ervar',
-    description: 'Tutaj będzie krótka historia postaci... kiedyś',
+    description:
+      'Młody, utalentowany czarodziej, który odrzucił bezpieczne mury akademii na rzecz prawdziwej przygody. Wychowanek wpływowego arcymaga, porzucił ścieżkę urzędnika, by zasmakować wolności. U boku Edgara, Theo i Brielli przeżył najpiękniejsze, ale i najbardziej tragiczne chwile życia — walcząc z demonami, maginiami krwi i potęgą Nurgla. Został agentem Protektoratu, wierząc w walkę o większe dobro. Jednak młodość i brawura go zgubiły — zginął rozdarty przez wojownika, którego napełnił miksturą wściekłości. Umarł zbyt wcześnie, z niedokończoną misją i sercem przepełnionym winą.',
     url: '/heroes/ervar.png',
   },
   {
-    name: 'Muriel',
-    description: 'Tutaj będzie krótka historia postaci... kiedyś',
+    name: 'Muriel z rodu Mariela',
+    description:
+      'Prawowita księżna Corvorosy, wygnana przez regenta i zdradzona przez możnych. W desperacji zawarła pakt z ruchem "Kolce Róży", marząc o odzyskaniu tronu. W krasnoludzkich kopalniach związała się z demonem, który przed laty przysiągł jej ojcu, że jego ród będzie rządził aż po kres księstwa. Opętana ideą przeznaczenia, zdradziła dawnych sprzymierzeńców, a z szalonym czarodziejem u boku doprowadziła do wojny domowej. Gdy odzyskała koronę, obłęd jej towarzysza zniszczył wszystko. Muriel przeżyła, lecz nadciągająca armia przypieczętowała los Corvorosy — tak spełniła się demoniczna obietnica: ród Mariela zasiadał na tronie do samego końca.',
     url: '/heroes/muriel.png',
   },
-
+  {
+    name: 'Edgar z rodu Lazarii',
+    description:
+      'Upadły szlachcic i złotousty oportunista, którego ambicje przerosły lojalność. Po kompromitującym incydencie w rodzinnym mieście stracił pozycję, ale nie porzucił marzeń o chwale. Niezrażony porażkami, wciąż podejmował ryzykowne akcje, co ostatecznie kosztowało go życie i duszę. Znany jako ocaleniec Korworosy, pogromca wiedźm z Głogówka i niedoszły partner Trzynastu Sióstr, przeszedł przez piekło — by ostatecznie zginąć z ręki najbardziej zaufanego towarzysza, Theo.',
+    url: '/heroes/edgar.png',
+  },
   {
     name: 'Theo',
     description: 'Tutaj będzie krótka historia postaci... kiedyś',
@@ -39,7 +47,8 @@ export const HEROES: Hero[] = [
   },
   {
     name: 'Joshua',
-    description: 'Tutaj będzie krótka historia postaci... kiedyś',
+    description:
+      'Był wojownikiem kaznodzieją z Salt Lake City. Gladiator, który potrafił zabić łyżeczką, jeśli tylko musiał. W jego oczach płonęło coś więcej niż gniew — wiara, że świat da się zmienić, nawet jeśli trzeba będzie spalić go po drodze.Nie pochodził stąd. Nie znał tych ludzi. Ale kiedy przyszło co do czego — walczył za nich, jakby byli jego braćmi. Umarł, jak żył — z wiarą w sercu i bronią w dłoni.Nie miał sztandaru, który by opłakiwał jego stratę. Nie miał pomnika, pieśni ani modlitwy. Ale miał przyjaciół. I to wystarczyło.',
     url: '/heroes/joshua.png',
   },
   {
