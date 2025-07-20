e5989fc52ebb3b814684c03cacb46a97d0d51ba5
Dolecki Patryk <pdolecki.it@gmail.com>
2025-07-20 10:01:18 +0200

    23: Removed bug with double progress spinner on login page

--- src/app/auth/login/login-form.ts
+++ src/app/auth/login/login-form.ts
@@ -10,7 +10,6 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { ReactiveFormsModule, FormBuilder } from '@angular/forms';
 import { Credentials } from '../../shared/interfaces/credentials';
 
@@ -22,7 +21,6 @@ import { Credentials } from '../../shared/interfaces/credentials';
     MatFormFieldModule,
     MatInputModule,
     MatIconModule,
-    MatProgressSpinnerModule,
   ],
   changeDetection: ChangeDetectionStrategy.OnPush,
   template: `
@@ -53,8 +51,6 @@ import { Credentials } from '../../shared/interfaces/credentials';
 
       @if (loginStatus()==='error') {
       <mat-error>Could not log you in with those details.</mat-error>
-      } @if (loginStatus() === 'loading') {
-      <mat-spinner diameter="50"></mat-spinner>
       }
 
       <button
@@ -77,9 +73,6 @@ import { Credentials } from '../../shared/interfaces/credentials';
       }
       mat-error {
         margin: 5px 0;
-      }
-      mat-spinner {
-        margin: 1rem 0;
       }
     `,
 })
